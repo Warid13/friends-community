688796c35c0402b9b5abe0458163c107b795df99
= <warid.bin.azad@gmail.com>
2020-07-26 22:40:58 +0600

    final done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './Component/Header/Header';
 import Users from './Component/Users/Users';
 function App() {
   return (
     <div className="body">
-      {/* <Header></Header> */}
       <Users></Users>
     </div>
   );
